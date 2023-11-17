a3a36c241f1cbcfcf676ee5bbd2fa79dd6b96627
Elisa Flemer <elisaflemer@gmail.com>
2023-11-16 23:24:27 -0300

    chatbot como nó

--- chatbot.py
+++ chatbot.py
@@ -1,68 +1,86 @@
-#! /bin/env python3
+#!/usr/bin/env python3
 import re
+import rclpy
+from rclpy.node import Node
 
+class ValletNode(Node):
+    def __init__(self):
+        super().__init__('vallet_node')
+        
+        self.destinos = {
+            r"\b(s?e?s?k?o?l)\b": "skol",
+            r"br[aa]?mm?a|brahma": "brahma",
+            r"ze|zé|zé delivery|delivery": "ze delivery",
+            r"b[ea]ts?|bats|beets|bites|beates|bates|betes|beats": "beats",
+            r"almo?x(?:erif(?:ado)?|arif(?:e|ado)?|cherif(?:ado|edo)?)": "almoxarifado",
+        }
 
-def greet_back():
-    print(f"Olá, eu sou o Vallet, o robô inteligente da cervejaria do futuro!")
+        self.coordenadas = {
+            "almoxarifado": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "beats": {"x": 1.26, "y": 0.0, "z": 0.0},
+            "brahma": {"x": 1.22, "y": -1.55, "z": 0.0},
+            "ze delivery": {"x": 0.62, "y": -0.81, "z": 0.0},
+            "skol": {"x": 0.0, "y": -1.55, "z": 0.0},
+        }
 
+        self.intent_dict = {
+            r"\b(?:[Oo][Ii]|ol[áa]|o[láa]|oii|oie)\b|\b(?:(?:[Bb]o[am])\s(tarde|dia|noite))": "greetings",
+            r"\b(?:[Tt]udo)?\s?(?:(?:[bB]em)|(?:[bB]ão)|(?:[fF]irme)|(?:em\sriba))\?": "genki",
+            r"\b(?:[Tt]chau|[Aa]deus|[Ff]ui|[Ss]air|[Ee]ncerrar|[Pp]arar|[Dd]esligar|[Ff]inalizar|[Ee]ncerrar|[Ee]ncerar|[Ee]ncerr)": "bye",
+            r"\b(ir para|me leve até|vai para|vai pra|vamos pra)\s+(\w+)": "go_to",
+        }
 
-def genki_back():
-    print("Comigo está tudo bem, e com você?")
+        self.action_dict = {
+            "greetings": self.greet_back,
+            "genki": self.genki_back,
+            "bye": self.bye,
+            "go_to": self.go_to,
+        }
 
+        self.get_logger().info("Vallet ROS 2 Node has been initialized.")
 
-def bye():
-    return "Tchau, até mais!"
-    exit()
+    def greet_back(self):
+        self.get_logger().info("Olá, eu sou o Vallet, o robô inteligente da cervejaria do futuro!")
 
+    def genki_back(self):
+        self.get_logger().info("Comigo está tudo bem!")
 
-def go_to():
-    for key, value in destinos.items():
-        pattern = re.compile(key)
-        groups = pattern.findall(command)
-        if groups:
-            print(f"OK, indo para {value}, em {coordenadas[value]}")
-            return
-    print("Não sei onde fica esse lugar")
+    def bye(self):
+        self.get_logger().info("Tchau, até mais!")
+        rclpy.shutdown()
+        exit()
 
-intent_dict = {
-    r"\b(?:[Oo][Ii]|ol[áa]|o[láa]|oii|oie)\b|\b(?:(?:[Bb]o[am])\s(tarde|dia|noite))": "greetings",
-    r"\b(?:[Tt]udo)?\s?(?:(?:[bB]em)|(?:[bB]ão)|(?:[fF]irme)|(?:em\sriba))\?": "genki",
-    r"\b(?:[Tt]chau|[Aa]deus|[Ff]ui|[Ss]air|[Ee]ncerrar|[Pp]arar|[Dd]esligar|[Ff]inalizar|[Ee]ncerrar|[Ee]ncerar|[Ee]ncerr)": "bye",
-    r"\b(ir para|me leve até)\s+(\w+)": "go_to",
-}
+    def go_to(self):
+        command = input("Digite o seu comando: ")
+        for key, value in self.destinos.items():
+            pattern = re.compile(key)
+            groups = pattern.findall(command)
+            if groups:
+                self.get_logger().info(f"OK, indo para {value}, em {self.coordenadas[value]}")
+                return
+        self.get_logger().info("Não sei onde fica esse lugar")
 
-action_dict = {
-    "greetings": greet_back,
-    "genki": genki_back,
-    "bye": bye,
-    "go_to": go_to,
-}
+    def run(self):
+        while rclpy.ok():
+            command = input("Digite o seu comando: ")
+            for key, value in self.intent_dict.items():
+                pattern = re.compile(key)
+                groups = pattern.findall(command)
+                if groups:
+                    self.action_dict[value]()
+                    break
+            else:
+                self.get_logger().info("Não entendi o que você quis dizer")
 
-destinos = {
-    r"\b(s?e?s?k?o?l)\b": "skol",
-    r"br[aa]?mm?a|brahma": "brahma",
-    r"ze|zé|zé delivery|delivery": "ze delivery",
-    r"b[ea]ts?|bats|beets|bites|beates|bates|betes|beats": "beats",
-    r"almo?x(?:erif(?:ado)?|arif(?:e|ado)?|cherif(?:ado|edo)?)": "almoxarifado",
-}
+            print()
 
-coordenadas = {
-    "almoxarifado": {"x": 0.0, "y": 0.0, "z": 0.0},
-    "beats": {"x": 1.26, "y": 0.0, "z": 0.0},
-    "brahma": {"x": 1.22, "y": -1.55, "z": 0.0},
-    "ze delivery": {"x": 0.62, "y": -0.81, "z": 0.0},
-    "skol": {"x": 0.0, "y": -1.55, "z": 0.0},
-}
+def main():
+    rclpy.init()
+    vallet_node = ValletNode()
+    vallet_node.run()
+    rclpy.spin(vallet_node)
+    vallet_node.destroy_node()
+    rclpy.shutdown()
 
-while True:
-    command = input("Digite o seu comando: ")
-    for key, value in intent_dict.items():
-        pattern = re.compile(key)
-        groups = pattern.findall(command)
-        if groups:
-            action_dict[value]()
-            break
-    else:
-        print("Não entendi o que você quis dizer")
-
-    print()
+if __name__ == '__main__':
+    main()
